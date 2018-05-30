dcfa5b18d9078644dce29ab308685b6b2af43d00
Pedro López Jiménez <otromasdeibi@gmail.com>
2018-05-30 19:05:41 +0200

    read phrase from the command line

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from translators import *
+import sys
 
-if __name__ == "__main__":
+if (__name__ == "__main__" and len(sys.argv) == 1):
     phraseToTranslate = input()
+elif (sys.argv[1] != ""):
+    phraseToTranslate = sys.argv[1]
 else:
-    phraseToTranslate = "frase base"    
+    print("Error reading input")    
 
 phraseTranslated = Translator(phraseToTranslate).translate()
 
