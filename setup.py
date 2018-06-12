d75b1eeb75864f9460edbd269effc89f71795aa8
Pedro López Jiménez <otromasdeibi@gmail.com>
2018-06-13 00:59:38 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='malenciano-translator',
+   version='1.0',
+   description='A module which allows you to translate into malenciano (joke about valencian language)',
+   author='Pedro López',
+   author_email='',
+   packages=['malenciano-translator'],  #same as name
+)
