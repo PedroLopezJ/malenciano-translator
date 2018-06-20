64885b915d61b8cc6370849832b2003f78c89325
Pedro López Jiménez <otromasdeibi@gmail.com>
2018-06-20 02:13:47 +0200

    tried to fix module import when using it as a package

--- malenciano_translator/translate.py
+++ malenciano_translator/translate.py
@@ -1,6 +1,11 @@
 import sys
+import os
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
 from translators import Translator
-sys.path.append('.')
 
 def getPhraseTranslated(phraseToTranslate):
     if (phraseToTranslate != ""):
